627d4ae925360f74820565ca38a3927c60157743
lukeyork16 <lukeyork16@gmail.com>
2026-07-22 17:32:19 -0600

    Update backtest.py

--- 02-trading-strategy-lab/backtest.py
+++ 02-trading-strategy-lab/backtest.py
@@ -6,17 +6,30 @@ def backtest(pricedata, signal):
     strategyreturn=signal.shift(1)*dailyreturn  #shift so we trade on yesterday's signal, no lookahead
     return strategyreturn
 
+#Our backtest for pairs trading. The signal applies long to stock 1 and short to stock 2
+def backtestpairs(data1, data2, signal):
+    return1=data1.pct_change()
+    return2=data2.pct_change()
+
+    #used if signal is 1 we will then long stock 1, short stock 2. if signal is -1 we will flip those
+    strategyreturn=signal.shift(1)*return1-signal.shift(1)*return2
+    return strategyreturn
+
 if __name__ == '__main__':
     from data import getdata, cleandata
-    from strategies import momentum, meanreversion_rsi, meanreversion_bollinger
+    from strategies import momentum, meanreversion_rsi, meanreversion_bollinger, pairstrade
 
-    df=getdata("SPY")
+    df=getdata(["SPY", "QQQ"])
     df=cleandata(df)
 
-    momentumreturns=backtest(df, momentum(df))
-    rsireturns=backtest(df, meanreversion_rsi(df))
-    bollingerreturns=backtest(df, meanreversion_bollinger(df))
+    momentumreturns=backtest(df["SPY"], momentum(df["SPY"]))
+    rsireturns=backtest(df["SPY"], meanreversion_rsi(df["SPY"]))
+    bollingerreturns=backtest(df["SPY"], meanreversion_bollinger(df["SPY"]))
+
+    pairsignal, zscore=pairstrade(df["SPY"], df["QQQ"])
+    pairsreturns=backtestpairs(df["SPY"], df["QQQ"], pairsignal)
 
     print("Momentum total return:", (1+momentumreturns).prod()-1)
     print("RSI total return:", (1+rsireturns).prod()-1)
     print("Bollinger total return:", (1+bollingerreturns).prod()-1)
+    print("Pairs total return:", (1+pairsreturns).prod()-1)
